56aed9b3f2621588b58229d14683cf07ed99ddb2
HyroVitalyProtago <hyrovitalyprotago@gmail.com>
2015-11-22 11:55:57 +0100

    v12-00

--- Lux & Nox/Assets/Scripts/MusicManager.cs
+++ Lux & Nox/Assets/Scripts/MusicManager.cs
@@ -1,26 +1,32 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class MusicManager : MonoBehaviour {
 
+    public static event Action OnIdle, OnLux, OnNox;
     static MusicManager Instance { get; set; }
 
     public static void Idle() {
         Change(Instance.idle);
+        OnIdle();
     }
     public static void Lux() {
         Change(Instance.lux);
+        OnLux();
     }
     public static void Nox() {
         Change(Instance.nox);
+        OnNox();
     }
     static void Change(AudioSource next) {
         next.time = Instance.current.time;
         Instance.current.Stop();
         next.Play();
         Instance.current = next;
+        Instance.transition.Play();
     }
 
-    public AudioSource idle, lux, nox;
+    public AudioSource idle, lux, nox, transition;
     AudioSource current;
 
     void Awake() {
