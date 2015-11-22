0020a252fc12490de56a8ad3ced19a57b05a87ea
rinj <rindra.andriamanefo@telecom-sudparis.eu>
2015-11-22 02:59:49 +0100

    Caméra, debug en y (à peu près)

--- Lux & Nox/Assets/Scripts/Camera2P.cs
+++ Lux & Nox/Assets/Scripts/Camera2P.cs
@@ -5,12 +5,18 @@ public class Camera2P : MonoBehaviour {
     [SerializeField]
     Transform player1, player2;
 
-    [SerializeField]
-    float minSizeX = 18f, minY = 0, minSizeXThreshold = 1.4f;
+	[SerializeField]
+	float minSizeX = 18f, minY = 0f;
+	
+	[SerializeField]
+	private float distanceMax = 7f;
+
+	[SerializeField]
+	private float sizeMin = 8f;
 
 	Vector3 middle;
 
-	public float xMargin = 5f;		// Distance delimiting the borders of the camera relatively to the players' positions
+	public float xMargin = 7f;		// Distance delimiting the borders of the camera relatively to the players' positions
 	public float xSmooth = 2f;		// How smoothly the camera catches up with it's target movement in the x axis.
 	public Vector2 maxXAndY;		// The maximum x and y coordinates the camera can have.
 	public Vector2 minXAndY;		// The minimum x and y coordinates the camera can have.
@@ -20,7 +26,7 @@ public class Camera2P : MonoBehaviour {
 
         Camera.main.transform.position = new Vector3(
             Mathf.Lerp(transform.position.x, middle.x, xSmooth * Time.deltaTime),
-            Mathf.Max(middle.y, minY),
+			Mathf.Max(middle.y, minY),
             Camera.main.transform.position.z
         );
     }
@@ -30,8 +36,11 @@ public class Camera2P : MonoBehaviour {
 		// multiplying by 0.5, because the ortographicSize is actually half the height
 		float width = Mathf.Abs(player1.position.x - player2.position.x) * 0.5f + 2 * xMargin;
 
+		if (Mathf.Abs (player1.position.x - player2.position.x) < distanceMax)
+			Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, sizeMin,- xSmooth);
 		// ... the target x coordinate should be a Lerp between the camera's current x position and the player's current x position.
-		Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize,
+		else
+			Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize,
 		                            Mathf.Max(width, minSizeX / 2f) * Screen.height / Screen.width, 
 		                            xSmooth * Time.deltaTime);
 
