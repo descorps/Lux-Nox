d3b4ae58ff0e32dbe7c5318143457eb02504698f
rinj <rindra.andriamanefo@telecom-sudparis.eu>
2015-11-22 01:01:26 +0100

    Caméra

--- Lux & Nox/Assets/Scripts/Camera2P.cs
+++ Lux & Nox/Assets/Scripts/Camera2P.cs
@@ -6,33 +6,44 @@ public class Camera2P : MonoBehaviour {
     Transform player1, player2;
 
     [SerializeField]
-    float minSizeY = 5f, minY = 0, minSizeXThreshold = 1.4f;
+    float minSizeX = 18f, minY = 0, minSizeXThreshold = 1.4f;
+
+	Vector3 middle;
+
+	public float xMargin = 10f;		// Distance delimiting the borders of the camera relatively to the players' positions
+	public float xSmooth = 2f;		// How smoothly the camera catches up with it's target movement in the x axis.
+	public Vector2 maxXAndY;		// The maximum x and y coordinates the camera can have.
+	public Vector2 minXAndY;		// The minimum x and y coordinates the camera can have.
 
     void SetCameraPos() {
         Vector3 middle = (player1.position + player2.position) * 0.5f;
 
         Camera.main.transform.position = new Vector3(
-            middle.x,
+            Mathf.Lerp(transform.position.x, middle.x, xSmooth * Time.deltaTime),
             Mathf.Max(middle.y, minY),
             Camera.main.transform.position.z
         );
     }
 
-    void SetCameraSize() {
-        // horizontal size is based on actual screen ratio
-        float minSizeX = (minSizeY * Screen.width / Screen.height) * minSizeXThreshold;
+	void TrackPlayers ()
+	{		
+		// multiplying by 0.5, because the ortographicSize is actually half the height
+		float width = Mathf.Abs(player1.position.x - player2.position.x) * 0.5f + 2 * xMargin;
 
-        // multiplying by 0.5, because the ortographicSize is actually half the height
-        float width = Mathf.Abs(player1.position.x - player2.position.x) * 0.5f;
-        float height = Mathf.Abs(player1.position.y - player2.position.y) * 0.5f;
+		// ... the target x coordinate should be a Lerp between the camera's current x position and the player's current x position.
+		Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize,
+		                            Mathf.Max(width, minSizeX / 2f) * Screen.height / Screen.width, 
+		                            xSmooth * Time.deltaTime);
 
-        // computing the size
-        float camSizeX = Mathf.Max(width, minSizeX);
-        Camera.main.orthographicSize = Mathf.Max(height, camSizeX * Screen.height / Screen.width, minSizeY);
-    }
+	}
+
+	void FixedUpdate ()
+	{
+		SetCameraPos();
+		TrackPlayers ();
+	}
+
 
     void Update() {
-        SetCameraPos();
-        SetCameraSize();
     }
 }
